1522a1cf0728e7905c598e0a05b5ac5be3cf3f26
remi Taylor <remi@remitaylor.com>
2015-09-03 15:14:04 -0700

    Shorten line in GCS sample for better readability on landing page

--- gcloud/storage.rb
+++ gcloud/storage.rb
@@ -8,7 +8,10 @@ storage = gcloud.storage
 
 bucket = storage.bucket "your-bucket-id"
 
-file = bucket.create_file "local/path/to/file.txt", "destination/path/file.txt"
+file = bucket.create_file(
+  "local/path/to/file.txt",
+  "destination/path/file.txt"
+)
 
 bucket.upload_file file
 # [END all]
